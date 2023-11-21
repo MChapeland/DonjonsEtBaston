3c22042975350ebaef83e27f931306c12046020c
MChapeland <matthieuchapeland@gmail.com>
2023-11-21 17:57:30 +0100

    rzerezr

---
+++ Project Insanity/Project/ProjectInsanity/ProjectInsanity/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace ProjectInsanity
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                tbHello.Text = "Stopped";
+                btnRun.Content = "Run";
+            }
+            else
+            {
+                tbHello.Text = "Running";
+                btnRun.Content = "Stop";
+            }
+
+            running = !running;
+
+        }
+    }
+}
